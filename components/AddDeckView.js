71bd599099ec37a0d295eef4d27ec404c07e5518
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-04-02 23:46:44 -0300

    feat: handle text to input deck

--- components/AddDeckView.js
+++ components/AddDeckView.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react'
-import { StyleSheet, KeyboardAvoidingView } from 'react-native'
-import { FormInput } from 'react-native-elements'
+import { StyleSheet, KeyboardAvoidingView, Keyboard } from 'react-native'
+import { FormInput, FormValidationMessage } from 'react-native-elements'
 import TextHeader from './TextHeader'
 import CustomButton from './CustomButton'
 import { black } from '../utils/colors'
 
 class AddDeckView extends Component {
 
+    state = {
+        title: '',
+        titleError: false
+    }
+
+    handleTitle = (title) => {
+        this.setState(() => ({
+            title,
+            titleError: (title === '' ? true: false)
+        }))
+    }
+
+    submit = () => {
+        const { title, titleError } = this.state
+
+        if (title === '') {
+            this.setState({
+                titleError: true
+            })
+        } else {
+            
+            // Save deck
+
+            // Dispatch action to update store
+
+            // Hide Keyboard
+            Keyboard.dismiss()
+
+            // Clear input
+            this.setState({
+                title: ''
+            })
+        }
+    }
+
     render() {
+
+        const { title, titleError } = this.state
+
         return(
             <KeyboardAvoidingView style={styles.container}>
                 <TextHeader>What is the title of your new Deck?</TextHeader>
                 <FormInput
                     placeholder='Enter the deck title here...'
                     inputStyle={styles.input}
+                    onChangeText={this.handleTitle}
+                    value={title}
                 />
+                {titleError && (
+                    <FormValidationMessage>This field is required</FormValidationMessage>
+                )}
                 <CustomButton
                     addStyle={{ backgroundColor: black, marginTop: 40 }}
+                    onPress={this.submit}
                 >
                     Create Deck
                 </CustomButton>
