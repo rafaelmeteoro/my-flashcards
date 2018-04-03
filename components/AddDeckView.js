a3725bf105cb8d1f5bccc19314cc5667cbd961b7
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-04-02 23:31:49 -0300

    feat: Create custom button and add colors

--- components/AddDeckView.js
+++ components/AddDeckView.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
 import { StyleSheet, KeyboardAvoidingView } from 'react-native'
+import { FormInput } from 'react-native-elements'
 import TextHeader from './TextHeader'
+import CustomButton from './CustomButton'
+import { black } from '../utils/colors'
 
 class AddDeckView extends Component {
 
     render() {
         return(
-            <KeyboardAvoidingView>
-                <TextHeader>What is the title of yor new Deck?</TextHeader>
+            <KeyboardAvoidingView style={styles.container}>
+                <TextHeader>What is the title of your new Deck?</TextHeader>
+                <FormInput
+                    placeholder='Enter the deck title here...'
+                    inputStyle={styles.input}
+                />
+                <CustomButton
+                    addStyle={{ backgroundColor: black, marginTop: 40 }}
+                >
+                    Create Deck
+                </CustomButton>
             </KeyboardAvoidingView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 50
+    },
+    input: {
+        textAlign: 'center',
+        fontSize: 18
+    }    
+})
 
 export default AddDeckView
